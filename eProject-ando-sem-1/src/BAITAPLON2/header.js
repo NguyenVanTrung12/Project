f0979e3da061105f2f9df2005b4b4d2dd29e6d73
Nguyen Van Trung <vantrung1242005@gmail.com>
2024-10-01 10:45:05 +0700

    first commit

---
+++ eProject-ando-sem-1/src/BAITAPLON2/header.js
@@ -0,0 +1,102 @@
+import { Link } from 'react-router-dom';
+import '../Css/header.css';
+import data from "../data.json";
+import { Children } from 'react';
+import { useState } from 'react';
+export default function Header({ name, ...props }) {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const handleToggle = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+    return (
+        <div className='body'>
+            <header>
+                <div className='header1'>
+                    <div className='logo'>
+                        <Link to="/" ><img src='./img/logo.png' alt='Logo' /></Link>
+                    </div>
+                    <div className="menu">
+                        <nav className="navbar navbar-expand-lg" style={{ padding: 0 }}>
+                            <Link to="/" className="navbar-brand" href="#"></Link>
+                            <button
+                                className="navbar-toggler text-bg-light"
+                                type="button"
+                                aria-expanded={isMenuOpen}
+                                aria-label="Toggle navigation"
+                                onClick={handleToggle}
+                            >
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className={`collapse navbar-collapse list ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link" aria-current="page">Home</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/Introduce" className="nav-link">Introduce</Link>
+                                    </li>
+
+                                    <li className="nav-item dropdown">
+                                        <a href="/New" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdowns" aria-expanded="false">
+                                            News
+                                        </a>
+                                        <ul className="dropdown-menu">
+                                            <Link to="/NewsBicycle" className="dropdown-item">Cycling</Link>
+                                            <Link to="/NewsSwimming" className="dropdown-item">Swimming</Link>
+                                            <Link to="/NewsFitness" className="dropdown-item">Sports & Fitness</Link>
+                                            <Link to="/NewGroupFitnes" className="dropdown-item">Group Exercise</Link>
+                                            <Link to="/NewFood" className="dropdown-item">Membership</Link>
+                                            <Link to="/NewSalos" className="dropdown-item">Salon & Spa</Link>
+                                        </ul>
+                                    </li>
+                                    <li className="nav-item dropdown">
+                                    <a href="/store" className="nav-link " role="button" data-bs-toggle="dropdowns" aria-expanded="false">
+                                            Store
+                                        </a>
+                                        {/* <a href="/Product" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdowns" aria-expanded="false">
+                                            Store
+                                        </a> */}
+                                        {/* <ul className="dropdown-menu">
+                                            <li><Link to="/" className="dropdown-item">Action</Link></li>
+                                            <li><Link to="/" className="dropdown-item">Another action</Link></li>
+                                            <li><Link to="/" className="dropdown-item">Something else here</Link></li>
+                                        </ul> */}
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link to="/Contact" className="nav-link" aria-disabled="true">Contact</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/about" className="nav-link" aria-disabled="true">About Us</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/FAQs" className="nav-link" aria-disabled="true" style={{textTransform: 'none'}}>FAQs</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/Register" className="nav-link" aria-disabled="true" style={{ marginRight: '15px' }}>Enquiry</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/Stiemap" className="nav-link" aria-disabled="true">Sitemap</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    {/* <div className="bonus">
+                        <div className='iconcart'>
+                            <i style={{ fontSize: '20px', marginRight: '20px', marginLeft: '15px' }} className='fa'>&#xf07a;</i>
+                        </div>
+                        <div className='login_menu'>
+                            <a href=''>Sign in</a>
+                        </div>
+                    </div> */}
+                </div>
+            </header>
+            <div className='banner_menu'></div>
+        </div>
+
+
+    );
+};
---
+++ eProject-ando-sem-1/src/BAITAPLON2/introduce.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import '../Css/introduce.css';
+import { Link } from 'react-router-dom';
+const Introduce = () => {
+    return (
+        <div className="container-fluid introduce">
+            <div className="container">
+            <nav className="breadcrumb">
+                    <Link class="nav-link" to="/"><span style={{ color: 'red' }}>Home </span>
+                        
+                        </Link> <span> » Introduce</span>
+                </nav>
+                <hr />
+                <div className="row">
+                    {/* Left Column */}
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-8 left">
+                        <h2>Introduce</h2>
+                        <h2>Going to the gym every day helps improve your physical fitness.</h2>
+                        <p>
+                            Nowadays, the need for beauty is increasing, so fitness centers are increasingly popular. But many people
+                            have the wrong idea that just following the videos on the internet is fine, practicing without a method
+                            will make you receive the opposite effects. Therefore, going to the gym/Fitness every day not only helps
+                            you have a beautiful body but also helps you improve your health, relieve your emotions more effectively.
+                            AE FITNESS today will share with readers the benefits of going to the gym to exercise every day.
+                        </p>
+                        <h2>Benefits of going to the gym every day</h2>
+                        <h2>Practice systematically</h2>
+                        <p>
+                            Of course, going to the gym will give you a more systematic workout than working out at home. Being able
+                            to interact with people and get exercise guidance will help you practice a healthier and more disciplined
+                            lifestyle. Not only that, working out with others will make you more interested in working out. Going to
+                            the gym will give you advice and guidance from knowledgeable trainers in the field of fitness, they will
+                            also help you balance your nutrition to make your exercises more effective.
+                        </p>
+                        <img src="./img/home-rigt-tintuc-2.jpg" alt="Gym Benefits" style={{ width: '100%' }} />
+                        <h2 style={{ marginTop: '30px' }}>Helps relieve stress effectively</h2>
+                        <p>
+                            Regular exercise and sports help people increase brain activity, relieve stress after a tiring day, or
+                            help you create more energy to start a new day. Regular exercise makes your body produce substances such
+                            as Dopamine, Epinephrine, which help bring back the importance of life. Of course, these things have been
+                            proven by scientists, and you can verify them on the web.
+                        </p>
+                    </div>
+
+                    {/* Right Column */}
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 rightt">
+                        <img src="./img/g31.jpg" alt="Article Image" className='imgg'  style={{ borderRadius: '10px' ,width:'100%' }} />
+                        <h4>Latest Articles</h4>
+                        <div className="news">
+                            {[
+                                {
+                                    img: './img/i4.jfif',
+                                    text: 'The importance of snacks for bodybuilders',
+                                },
+                                {
+                                    img: './img/i1.jfif',
+                                    text: 'How to get a toned body',
+                                },
+                                {
+                                    img: './img/i5.jfif',
+                                    text:
+                                        "Don't miss 5 leg exercises to help your legs be strong and your leg muscles are strong",
+                                },
+                                {
+                                    img: './img/i3.jfif',
+                                    text: 'Surprising way to reduce facial fat effectively with a scientific diet',
+                                },
+                                {
+                                    img: './img/i6.jpg',
+                                    text: 'Not one exercising in cold weather',
+                                },
+                            ].map((article, index) => (
+                                <div className="row" key={index}>
+                                    <div className="col-sx-4 col-sm-4 col-md-4 col-lg-4 avatarr">
+                                        <img src={article.img} alt="Article Avatar" className="avatar"  />
+                                    </div>
+                                    <div className="col-sx-8 col-sm-8 col-md-8 col-lg-8">
+                                        <p>{article.text}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="frm">
+                            <form>
+                                <h3 style={{ marginLeft: '10px' }}>Sign up for a quote</h3>
+                                <p style={{ marginLeft: '10px' }}>Please fill in all information and send it to us</p>
+                                <input type="text" placeholder="Full name..." />
+                                <input type="text" placeholder="Phone number..." />
+                                <input type="text" placeholder="Address..." />
+                                <textarea type="text" placeholder="Additional notes..."></textarea>
+                                <button type="submit">Sign up for a quote</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Introduce;
